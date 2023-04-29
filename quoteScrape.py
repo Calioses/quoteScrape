453ada566d2aa361ba02bbc279e60befacc783fe
Monkeydew <55723201+Calioses@users.noreply.github.com>
2023-04-29 00:17:59 -0400

    Final

--- quoteScrape.py
+++ quoteScrape.py
@@ -13,17 +13,18 @@ def quoteScrape(url):
     while True:
         # first sleep for initial page load
         time.sleep(1)
-        # Not really sure what was going on wit the previous selectors. Selenium has a bug where several By.<options> are defaulting to CSS selector so I've been strong armed.
+        # Not really sure what was going on wit the previous selectors.
+        # Selenium has a bug where several By.<options> are defaulting to CSS selector so I've been strong armed into using them.
         first_quote = driver.find_element(
             By.XPATH, '/html/body/div/div[2]').text
         next_button = driver.find_element(
             By.CSS_SELECTOR, 'body > div > nav > ul > li > a')
-        # My machine needs the second sleep for selenium to work write. Not need everywhere. Just a budget fix for a silly problem
+        # My machine needs the second sleep for selenium to work right. Not need everywhere. Just a budget fix for a silly problem
         time.sleep(1)
         # changed the logic to make a little more sense.
         if not next_button:
-            break
-        # It's good habit to use action chain to click as it will avoid pain with what tag actual holds the button. Not to mention bot protections
+            return  # changed from break to return as I put it in a function
+        # It's good habit to use action chain to click as it will avoid pain with, what tag actual holds the button. Not to mention bot protections.
         webdriver.ActionChains(driver).click(next_button).perform()
         # No need to change
         print(first_quote)
